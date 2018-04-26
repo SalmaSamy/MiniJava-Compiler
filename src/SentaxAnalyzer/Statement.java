790615e6de43de0ec5688a71397cb43479bd537f
Salma Samy <salma.al.emam96@gmail.com>
2018-04-26 17:25:11 +0200

    new classes Sysout, MemberAssignment

--- src/SentaxAnalyzer/Statement.java
+++ src/SentaxAnalyzer/Statement.java
@@ -5,10 +5,9 @@ public class Statement {
 	 * Statement = "{" Statments "}"
 	 * | IfCondition
 	 * | While
-	 * |"System.out.println" "(" Expression ")" ";"
-	 * | Identifier SquareEx "=" Expression ";"
+	 * | SysOut
+	 * | memberAssignment
 	
-	 * SquareEx = “[“ Expression “]” | e
 	 * Statments = Statement Statments | e
 	 */
 
@@ -31,75 +30,19 @@ public class Statement {
 		}
 		Analyzer.index--;
 		
-		//"System.out.println" "(" Expression ")" ";"
-		Node sysout = Analyzer.addTerminalNode("SYSTEM.OUT.PRINTLN");
+		//Sysout
+		Node sysout = SysOut.valid();
 		if(sysout != null){
-			statement.addChild(sysout);
-			
-			Node LRound = Analyzer.addTerminalNode("LEFT_ROUND_B");
-			if(LRound == null)
-				return null;
-			statement.addChild(LRound);
-			
-			Node expression = Expression.valid();
-			if(expression == null)
-				return null;
-			statement.addChild(expression);
-			
-			Node RRound = Analyzer.addTerminalNode("RIGHT_ROUND_B");
-			if(RRound == null)
-				return null;
-			statement.addChild(RRound);
-			
-			Node semicolon = Analyzer.addTerminalNode("SEMICOLON");
-			if(semicolon == null)
-				return null;
-			statement.addChild(semicolon);
-			
+			statement.addChild(sysout);			
 			return statement;
 		}
 		Analyzer.index--;
 		
-		//Identifier SquareEx "=" Expression ";"
-		Node idNode = Analyzer.addTerminalNode("ID");
-		if (idNode != null) {
-			statement.addChild(idNode);
-			
-			//SquareEx = “[“ Expression “]” | e
-			Node LSQUARE = Analyzer.addTerminalNode("LEFT_SQUARE_B");
-			if (LSQUARE != null){
-				statement.addChild(LSQUARE);
-
-				Node expression = Expression.valid();
-				if (expression == null)
-					return null;
-				statement.addChild(expression);
-
-				Node RSQUARE = Analyzer.addTerminalNode("RIGHT_SQUARE_B");
-				if (RSQUARE == null)
-					return null;
-				statement.addChild(RSQUARE);
-			}
-			else
-				Analyzer.index--;
-			
-			Node equal = Analyzer.addTerminalNode("ASSIGNMENT");
-			if(equal == null)
-				return null;
-			statement.addChild(equal);
-			
-			Node expression = Expression.valid();
-			if (expression == null)
-				return null;
-			statement.addChild(expression);
-			
-			
-			Node semicolon = Analyzer.addTerminalNode("SEMICOLON");
-			if(semicolon == null)
-				return null;
-			statement.addChild(semicolon);
+		//MemberAssignment
+		Node memberAssignment = MemberAssignment.valid();
+		if (memberAssignment != null) {
+			statement.addChild(memberAssignment);
 			return statement;
-
 		}
 		Analyzer.index--;
 		
