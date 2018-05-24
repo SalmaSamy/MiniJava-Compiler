aa9ef374df592c42e1d092373ae3bdc7f27f24b1
Seif Gamal <seif_gamal09@yahoo.com>
2018-05-24 22:28:12 +0200

    done

---
+++ src/rules/Expression.java
@@ -0,0 +1,87 @@
+package rules;
+
+import lexical_Analyzer.TokenType;
+import sentax_Analyzer.Node;
+import sentax_Analyzer.Parser;
+
+public class Expression {
+
+	/*
+	 * Expression = Term LaLa OperatorSide LaLa = "." AfterDot | "[" Expression
+	 * "]" | e OperatorSide = Operators Expression | e
+	 * 
+	 * AfterDot = "length” | Identifier "(" ArgumentParameter ")"
+	 */
+
+	public static Node valid() {
+		Node expression = new Node("Expression");
+
+		// Term
+		Node term = Term.valid();
+		expression.addChild(term);
+
+		if (term.isException())
+			return expression;
+
+		// LaLa
+		Node dot = Parser.addTerminalNode(TokenType.DOT, true);
+		if (dot != null) {
+			expression.addChild(dot);
+
+			// function call
+			Node functionCall = FunctionCall.valid();
+			if (functionCall != null) {
+				expression.addChild(functionCall);
+<<<<<<< HEAD
+			} else {
+				--Parser.index;
+
+=======
+			}
+			else{
+				--Parser.index;
+>>>>>>> 0539365a71dc943231a79e2b253241f5f29af2f3
+				// .length
+				Node len = Parser.addTerminalNode(TokenType.LENGTH, false);
+				expression.addChild(len);
+			}
+<<<<<<< HEAD
+		} else
+=======
+		}
+		else
+>>>>>>> 0539365a71dc943231a79e2b253241f5f29af2f3
+			--Parser.index;
+
+		// [expression]
+		Node LSquare = Parser.addTerminalNode(TokenType.LEFT_SQUARE_B, true);
+		if (LSquare != null) {
+			expression.addChild(LSquare);
+
+			Node inExpression = Expression.valid();
+			expression.addChild(inExpression);
+
+			if (inExpression.isException())
+				return expression;
+
+			Node RSquare = Parser.addTerminalNode(TokenType.RIGHT_SQUARE_B, false);
+			expression.addChild(RSquare);
+
+		} else
+			--Parser.index;
+
+		// operator expression
+		Node operator = Operators.valid();
+
+		if (operator != null) {
+			expression.addChild(operator);
+
+			Node opExpression = Expression.valid();
+			expression.addChild(opExpression);
+		} else
+			--Parser.index;
+
+		return expression;
+	}
+
+}
---
+++ src/sentax_Analyzer/Parser.java
@@ -0,0 +1,86 @@
+package sentax_Analyzer;
+
+import java.util.ArrayList;
+
+import lexical_Analyzer.Token;
+import lexical_Analyzer.TokenType;
+import rules.Goal;
+
+public class Parser {
+	private static ArrayList<Token> tokens;
+	public static int index = 0;
+
+	public Parser(ArrayList<Token> t) {
+		tokens = new ArrayList<Token>();
+
+		// remove \n and comments.
+		for (Token token : t) {
+			if (token.type == TokenType.EOL || token.type == TokenType.S_COMMENTS || token.type == TokenType.M_COMMENTS)
+				continue;
+
+			tokens.add(token);
+		}
+	}
+
+	public static Token getCurToken() {
+		if (index == tokens.size()) {
+			return null;
+		}
+
+		Token token = Parser.tokens.get(index);
+		index++;
+		return token;
+	}
+
+	public void parse() {
+		Node root = Goal.valid();
+		print(root);
+	}
+
+	// print leaf nodes only.
+	public static void print(Node cur) {
+
+		if (cur.isLeaf() && !cur.isEpsilon()) {
+			if (cur.isException())
+				System.out.println("\nException: " + cur.getName());
+			else
+				System.out.print(cur.getName() + " ");
+			if (cur.getName().equals(";") || cur.getName().equals("}") || cur.getName().equals("{"))
+				System.out.println();
+		}
+		for (Node node : cur.getChildren()) {
+			print(node);
+		}
+	}
+
+	// just for testing ..
+	public static void test() {
+		System.out.println(Parser.tokens.get(index).type.name());
+	}
+
+	public static Node addTerminalNode(TokenType expected, boolean optional) {
+		Token token = Parser.getCurToken();
+		// TODO Analyzer.intdex--
+
+		if (token == null || token.type != expected) {
+			if (optional)
+				return null;
+
+			if (token == null) {
+				Node ret = new Node("Expected More tokens");
+				ret.setException(true);
+				return ret;
+			}
+
+			Node ret = new Node("Expected: " + expected.name() + " found: " + token.value);
+			ret.setException(true);
+			return ret;
+		}
+
+		Node idNode = new Node(expected.name());
+		Node terminalNode = new Node(token.value);
+		idNode.addChild(terminalNode);
+
+		return idNode;
+	}
+}
